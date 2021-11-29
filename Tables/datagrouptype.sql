3ef69546935cee52d7cf071cea7e3884c3cfed73
Matt Kernicky <83383674+mattkernicky@users.noreply.github.com>
2021-11-28 20:56:19 -0500

    Delete Tables directory

--- Tables/datagrouptype.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists `datagrouptype`;
-create table `datagrouptype` (
-  `DataGroupTypeId` int not null auto_increment,
-  `DataGroupTypeName` varchar(50) default null,
-  `Description` varchar(1000) default null,
-  primary key (`datagrouptypeid`),
-  unique key `datagrouptype_datagrouptypename_unique` (`DataGroupTypeName`)
-) engine=InnoDB auto_increment=7 default charset=utf8mb4 collate=utf8mb4_0900_ai_ci;
